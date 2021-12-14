2ee8866fa88cd4ee1d13842f43d533f60625272b
amolsoft123 <badeas999@gmail.com>
2021-12-14 15:18:11 -0800

    add lower case feature

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -8,8 +8,9 @@ def analyze(request):
     djtext = request.GET.get('text', 'default')
     removepunc=request.GET.get('removepunc', 'off')
     fullcaps=request.GET.get('fullcaps', 'off')
+    lowercaps=request.GET.get('lowercaps', 'off')
     #analyzed = djtext
-    
+
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
@@ -25,6 +26,14 @@ def analyze(request):
             analyzed = analyzed + char.upper()
         params = {'purpose':"Change To Uppercase", 'analyzed_text':analyzed}
         return render(request, 'analyze.html', params)
+
+    elif(lowercaps=="on"):
+        analyzed= ""
+        for char in djtext:
+            analyzed = analyzed + char.lower()
+        params = {'purpose':"Change To Lowercase", 'analyzed_text':analyzed}
+        return render(request, 'analyze.html', params)
+
 
     else:
         return HttpResponse('Error')
